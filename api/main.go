9e51c8e8f581446bc5463785a21b0e77fc24305a
Julia Evans <julia@jvns.ca>
2021-09-27 15:39:15 -0400

    add skeleton dns server

---
+++ api/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/miekg/dns"
+	"log"
+	"net"
+	"strconv"
+)
+
+var domainsToAddresses map[string]string = map[string]string{
+	"google.com.":       "1.2.3.4",
+	"jameshfisher.com.": "104.198.14.52",
+}
+
+type handler struct{}
+
+func (this *handler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
+	msg := dns.Msg{}
+	msg.SetReply(r)
+	switch r.Question[0].Qtype {
+	case dns.TypeA:
+		msg.Authoritative = true
+		domain := msg.Question[0].Name
+		address, ok := domainsToAddresses[domain]
+		if ok {
+			msg.Answer = append(msg.Answer, &dns.A{
+				Hdr: dns.RR_Header{Name: domain, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 60},
+				A:   net.ParseIP(address),
+			})
+		}
+	}
+	w.WriteMsg(&msg)
+}
+
+func main() {
+	srv := &dns.Server{Addr: ":" + strconv.Itoa(53), Net: "udp"}
+	srv.Handler = &handler{}
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf("Failed to set udp listener %s\n", err.Error())
+	}
+}

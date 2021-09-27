22f9d3604ff65aa63f2e5cb69215e6854b2bf3ab
Julia Evans <julia@jvns.ca>
2021-09-27 16:42:50 -0400

    host own A records?

--- api/main.go
+++ api/main.go
@@ -10,8 +10,10 @@ import (
 )
 
 var domainsToAddresses map[string]string = map[string]string{
-	"google.com.":       "1.2.3.4",
-	"jameshfisher.com.": "104.198.14.52",
+	"google.com.":          "1.2.3.4",
+	"jameshfisher.com.":    "104.198.14.52",
+	"ns1.messwithdns.com.": "213.188.214.237",
+	"ns2.messwithdns.com.": "213.188.214.254",
 }
 
 type handler struct{}
@@ -22,6 +24,16 @@ func (this *handler) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
 	fmt.Println("Received request: ", r.Question[0].String())
 	switch r.Question[0].Qtype {
 	case dns.TypeA:
+		msg.Authoritative = true
+		domain := msg.Question[0].Name
+		address, ok := domainsToAddresses[domain]
+		if ok {
+			msg.Answer = append(msg.Answer, &dns.A{
+				Hdr: dns.RR_Header{Name: domain, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 60},
+				A:   net.ParseIP(address),
+			})
+		}
+	case dns.TypeNS:
 		msg.Authoritative = true
 		domain := msg.Question[0].Name
 		address, ok := domainsToAddresses[domain]

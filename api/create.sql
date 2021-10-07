e47d9dea4acc07ecc27b9b96e4daccd4a5791974
Julia Evans <julia@jvns.ca>
2021-10-07 10:39:17 -0400

    sql

---
+++ api/create.sql
@@ -0,0 +1,11 @@
+CREATE TABLE dns_records 
+(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  rrtype INTEGER NOT NULL,
+  ttl INTEGER NOT NULL,
+  content TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+);
+-- unique constraint on name, rrtype, content
+CREATE UNIQUE INDEX dns_records_name_rrtype_content_uindex ON dns_records (name, rrtype, content);

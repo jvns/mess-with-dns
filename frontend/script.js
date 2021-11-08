7ab159ac259c70b6ea7d0e4acf192c838001685c
Julia Evans <julia@jvns.ca>
2021-11-08 14:54:55 -0500

    wip

--- frontend/script.js
+++ frontend/script.js
@@ -2,6 +2,90 @@ Vue.use(VueFormulate)
 
 console.log('Hello World');
 
+const rrTypes = {
+    'A': 1,
+    'NS': 2,
+    'MD': 3,
+    'MF': 4,
+    'CNAME': 5,
+    'SOA': 6,
+    'MB': 7,
+    'MG': 8,
+    'MR': 9,
+    'NULL': 10,
+    'WKS': 11,
+    'PTR': 12,
+    'HINFO': 13,
+    'MINFO': 14,
+    'MX': 15,
+    'TXT': 16,
+    'RP': 17,
+    'AFSDB': 18,
+    'X25': 19,
+    'ISDN': 20,
+    'RT': 21,
+    'NSAP': 22,
+    'NSAP-PTR': 23,
+    'SIG': 24,
+    'KEY': 25,
+    'PX': 26,
+    'GPOS': 27,
+    'AAAA': 28,
+    'LOC': 29,
+    'NXT': 30,
+    'EID': 31,
+    'NIMLOC': 32,
+    'SRV': 33,
+    'ATMA': 34,
+    'NAPTR': 35,
+    'KX': 36,
+    'CERT': 37,
+    'A6': 38,
+    'DNAME': 39,
+    'SINK': 40,
+    'OPT': 41,
+    'APL': 42,
+    'DS': 43,
+    'SSHFP': 44,
+    'IPSECKEY': 45,
+    'RRSIG': 46,
+    'NSEC': 47,
+    'DNSKEY': 48,
+    'DHCID': 49,
+    'NSEC3': 50,
+    'NSEC3PARAM': 51,
+    'TLSA': 52,
+    'SMIMEA': 53,
+    'HIP': 55,
+    'NINFO': 56,
+    'RKEY': 57,
+    'TALINK': 58,
+    'CDS': 59,
+    'CDNSKEY': 60,
+    'OPENPGPKEY': 61,
+    'CSYNC': 62,
+    'SPF': 99,
+    'UINFO': 100,
+    'UID': 101,
+    'GID': 102,
+    'UNSPEC': 103,
+    'NID': 104,
+    'L32': 105,
+    'L64': 106,
+    'LP': 107,
+    'EUI48': 108,
+    'EUI64': 109,
+    'TKEY': 249,
+    'TSIG': 250,
+    'IXFR': 251,
+    'AXFR': 252,
+    'MAILB': 253,
+    'MAILA': 254,
+    'ANY': 255,
+    'URI': 256,
+    'CAA': 257,
+}
+
 const schemas = {
     'A': [{
             'name': 'name',
@@ -9,9 +93,12 @@ const schemas = {
             'validation': 'required',
         },
         {
-            'name': 'ipv4_address',
+            'name': 'A',
             'label': 'IPv4 Address',
-            'validation': 'required',
+            'validation': "matches:/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\/",
+            'validation-messages': {
+                'matches': 'Invalid IPv4 Address'
+            },
         },
     ],
     'AAAA': [{
@@ -20,13 +107,13 @@ const schemas = {
             'validation': 'required',
         },
         {
-            'name': 'ipv6_address',
+            'name': 'AAAA',
             'label': 'IPv6 Address',
             'validation': 'required',
         },
     ],
     'CNAME': [{
-            'name': 'name',
+            'name': 'CNAME',
             'label': 'Name',
             'validation': 'required',
         },
@@ -86,6 +173,31 @@ Vue.component('new-record', {
             type: 'A',
         }
     },
+
+    methods: {
+        createRecord: function(data) {
+            console.log(data);
+            // { "type": "A", "name": "help", "A": "1.2.3.4" }
+            // =>
+            // { "Hdr": { "Name": "example.messwithdns.com.", "Rrtype": 1, "Class": 1, "Ttl": 5, "Rdlength": 0 }, "A": "93.184.216.34" }
+            var record = {
+                "Hdr": {
+                    "Name": data.name,
+                    "Rrtype": rrTypes[data.type],
+                    "Class": 1,
+                    "Ttl": data.ttl,
+                    "Rdlength": 0,
+                },
+            }
+            // copy rest of fields from data
+            for (var key in data) {
+                if (key != 'name' && key != 'type' && key != 'ttl') {
+                    record[key] = data[key];
+                }
+            }
+            console.log(record);
+        },
+    }
 });
 
 var app = new Vue({

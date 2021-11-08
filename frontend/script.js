58c12a1496195a9906fc6175aaa8d7dab757fddd
Julia Evans <julia@jvns.ca>
2021-11-08 13:28:50 -0500

    wip

--- frontend/script.js
+++ frontend/script.js
@@ -1,9 +1,24 @@
 Vue.use(VueFormulate)
 
 console.log('Hello World');
+
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        type: 'A',
+        schemas: {
+            'A': [{
+                    'name': 'name',
+                    'label': 'Name',
+                    'validation': 'required',
+                },
+                {
+                    'name': 'ipv4_address',
+                    'label': 'IPv4 Address',
+                    'validation': 'required',
+                },
+            ],
+        },
     }
 });

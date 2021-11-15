c926679ec9df023aa45fb793b0965090d682f6f7
Julia Evans <julia@jvns.ca>
2021-11-15 15:11:06 -0500

    add tests

--- frontend/test/mess.spec.js
+++ frontend/test/mess.spec.js
@@ -1,7 +1,7 @@
 // example.spec.js
 const { test, expect } = require('@playwright/test');
 
-test('snapshot looks ok', async ({ page }) => {
+test('homepage snapshot looks ok', async ({ page }) => {
   const browserContext = page.context();
   await browserContext.addInitScript({
     path: 'preload.js'
@@ -10,4 +10,27 @@ test('snapshot looks ok', async ({ page }) => {
   await page.goto('http://localhost:8080');
 
   expect(await page.screenshot()).toMatchSnapshot('index.png');
+});
+
+test('screenshot of random page looks ok', async ({ page }) => {
+  const browserContext = page.context();
+  await browserContext.addInitScript({
+    path: 'preload.js'
+  });
+
+  await page.goto('http://localhost:8080/#banana-boat');
+
+  expect(await page.screenshot()).toMatchSnapshot('banana-boat.png');
+});
+
+test('subdomain page snapshot', async ({ page }) => {
+  const browserContext = page.context();
+  await browserContext.addInitScript({
+    path: 'preload.js'
+  });
+
+  await page.goto('http://localhost:8080');
+  await page.click('#randomSubdomain', {timeout: 1000});
+
+  expect(await page.screenshot()).toMatchSnapshot('random-subdomain.png');
 });

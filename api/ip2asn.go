6cbd2dab6b42c7c368d910385a4a777af6e72529
Julia Evans <julia@jvns.ca>
2021-11-22 21:19:07 -0500

    add some ASN parsing code

---
+++ api/ip2asn.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+    "bufio"
+    "bytes"
+    "errors"
+    "net"
+    "os"
+    "strconv"
+    "strings"
+)
+
+type IPRange struct {
+    StartIP net.IP
+    EndIP net.IP
+    Num int
+    Name string
+    Country string
+}
+
+func parseInt(s string) int {
+    i, err := strconv.Atoi(s)
+    if err != nil {
+        return 0
+    }
+    return i
+}
+
+func FindASN(lines []IPRange, ip net.IP) (IPRange, error) {
+    // binary search
+    start := 0
+    end := len(lines) - 1
+    for start <= end {
+        mid := (start + end) / 2
+        // check if it's between StartIP and EndIP
+        if bytes.Compare(ip, lines[mid].StartIP) >= 0 && bytes.Compare(ip, lines[mid].EndIP) <= 0 {
+            return lines[mid], nil
+        } else if bytes.Compare(ip, lines[mid].StartIP) < 0 {
+            end = mid - 1
+        } else {
+            start = mid + 1
+        }
+    }
+    return IPRange{}, errors.New("not found")
+}
+
+func ReadASNs(filename string) ([]IPRange, error) {
+    f, err := os.Open(filename)
+    if err != nil {
+        return nil, err
+    }
+    defer f.Close()
+    // read lines
+    scanner := bufio.NewScanner(f)
+    var lines []IPRange
+    for scanner.Scan() {
+        line := scanner.Text()
+        // split line
+        fields := strings.Split(line, "\t")
+        // parse fields
+        IPRange := IPRange{
+            StartIP: net.ParseIP(fields[0]),
+            EndIP: net.ParseIP(fields[1]),
+            Num: parseInt(fields[2]),
+            Country: fields[3],
+            Name: fields[4],
+        }
+        lines = append(lines, IPRange)
+    }
+    return lines, nil
+}
--- api/parse_test.go
+++ api/parse_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"testing"
+    "net"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -15,5 +16,12 @@ func TestParseMX(t *testing.T) {
 func TestInvalidFqdn(t *testing.T) {
 	jsonString := `{"Hdr":{"Name":"example.com.","Rrtype":15,"Class":1,"Ttl":3600,"Rdlength":0},"Preference":10,"Mx":"mail.example.com"}`
 	_, err := ParseRecord([]byte(jsonString))
-	assert.Equal(t, err.Error(), "Invalid RR: dns: domain must be fully qualified")
+	assert.Equal(t, err.Error(), "Invalid RR: dns: domain must be fully qualified, &dns.MX{Hdr:dns.RR_Header{Name:\"example.com.\", Rrtype:0xf, Class:0x1, Ttl:0xe10, Rdlength:0x0}, Preference:0xa, Mx:\"mail.example.com\"}")
+}
+
+func TestParseASN(t *testing.T) {
+    ranges, _ := ReadASNs("../ip2asn-v4.tsv")
+    r, _ := FindASN(ranges, net.ParseIP("172.217.13.174"))
+    assert.Equal(t, r.Num, 15169)
+    assert.Equal(t, r.Name, "GOOGLE - Google LLC")
 }

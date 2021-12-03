36d2c6b8c88a59f42389c95a327ac201e67cc190
Julia Evans <julia@jvns.ca>
2021-12-02 22:09:45 -0500

    make the modal different

--- frontend/components/Experiments.js
+++ frontend/components/Experiments.js
@@ -2,6 +2,24 @@ import template from './Experiments.html';
 export default {
     template: template,
     props: ['domain', 'subdomain'],
+    mounted: function() {
+        document.querySelectorAll('.experiment').forEach((exp) => {
+            const summary = exp.querySelector('summary');
+            const overlay = '<div class="details-modal-overlay"></div>'
+            summary.insertAdjacentHTML('beforeend', overlay);
+            const title = summary.innerText.split("\n")[0];
+            const modal = exp.querySelector('.details-modal');
+            const html = modal.innerHTML;
+            modal.innerHTML = `
+                <div class="details-modal-title"> ${title} </div>
+                <div class="details-modal-close"> </div>
+                <div class="details-modal-content">
+                    ${html}
+                </div>
+            `;
+        });
+        console.log('hi');
+    },
     methods: {
         currDomain: function() {
             return this.domain || 'your-domain';

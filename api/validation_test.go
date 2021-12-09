b06af7e82308c228bbd60571ea032258c372e01f
Julia Evans <julia@jvns.ca>
2021-12-09 17:45:39 -0500

    fix typo in test

--- api/validation_test.go
+++ api/validation_test.go
@@ -7,25 +7,25 @@ import (
 )
 
 func TestValidate(t *testing.T) {
-	err := subdomainError("www.messwithdns.net", "www")
+	err := validateDomainName("www.messwithdns.net", "www")
 	assert.NotNil(t, err, "must be fully qualified")
 
-	err = subdomainError("www.messwithdns.net.", "www")
+	err = validateDomainName("www.messwithdns.net.", "www")
 	assert.NotNil(t, err, "www is invalid")
 
-	err = subdomainError("test.a.b.www.messwithdns.net.", "www")
+	err = validateDomainName("test.a.b.www.messwithdns.net.", "www")
 	assert.NotNil(t, err, "www is invalid")
 
-	err = subdomainError("asdf.messwithdns.net.asdf.messwithdns.net.", "asdf")
+	err = validateDomainName("asdf.messwithdns.net.asdf.messwithdns.net.", "asdf")
 	assert.NotNil(t, err, "messwithdns occurs twice")
 
-	err = subdomainError("x..messwithdns.net.", "asdf")
+	err = validateDomainName("x..messwithdns.net.", "asdf")
 	assert.NotNil(t, err, "invalid domain name")
 
-	err = subdomainError("asdf.test.messwithdns.net.", "test")
+	err = validateDomainName("asdf.test.messwithdns.net.", "test")
 	assert.Nil(t, err)
 
-	err = subdomainError("a.b.c.d.messwithdns.net.", "d")
+	err = validateDomainName("a.b.c.d.messwithdns.net.", "d")
 	assert.Nil(t, err)
 }
 

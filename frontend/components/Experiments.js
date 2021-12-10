f606799b978199c3547759643bf5adb52aa2182d
Julia Evans <julia@jvns.ca>
2021-12-10 10:51:22 -0500

    fix broken titles????? why???

--- frontend/components/Experiments.js
+++ frontend/components/Experiments.js
@@ -7,11 +7,11 @@ export default {
             const summary = exp.querySelector('summary');
             const overlay = '<div class="details-modal-overlay"></div>'
             summary.insertAdjacentHTML('beforeend', overlay);
-            const title = summary.innerText.split("\n")[0];
+            const h3 = exp.querySelector('summary h3');
             const modal = exp.querySelector('.details-modal');
             const html = modal.innerHTML;
             modal.innerHTML = `
-                <div class="details-modal-title"> ${title} </div>
+                <div class="details-modal-title"> ${h3.innerHTML} </div>
                 <div class="details-modal-close"> </div>
                 <div class="details-modal-content">
                     ${html}

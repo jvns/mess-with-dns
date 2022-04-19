0b0d29bfb700146b28de333b15bbff6eac26b1b9
Julia Evans <julia@jvns.ca>
2022-04-19 12:34:12 -0400

    use tiny tailwind file

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+ purge: {
+  enabled: true,
+  content: ["./**.{html,js}"],
+ },
+ theme: {},
+ variants: {},
+ plugins: [],
+}
--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ set -eu
 ls api/*.go api/go* scripts/* run.sh | entr -r bash scripts/run_go.sh &
 cd frontend
 ls ./*.js ./*.ts components/* | grep -v bundle | entr bash esbuild.sh &
+npx tailwindcss -o css/tailwind-small.min.css --watch &
 
 trap 'kill $(jobs -p)' SIGINT SIGTERM
 

673db252feb04778948b1855e5f8cb9ada191a42
Julia Evans <julia@jvns.ca>
2023-11-01 15:14:03 -0400

    add an index on dns_requests (created_at)

--- api/create.sql
+++ api/create.sql
@@ -35,3 +35,4 @@ CREATE TABLE IF NOT EXISTS subdomains (
 
 CREATE INDEX IF NOT EXISTS dns_requests_name_uindex ON dns_requests (name);
 CREATE INDEX IF NOT EXISTS dns_requests_subdomain_uindex ON dns_requests (subdomain);
+CREATE INDEX IF NOT EXISTS dns_requests_created_at ON dns_requests (created_at);

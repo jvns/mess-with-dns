d414cf3d35e4b37be0effaea924e7e97ae9fc6d9
Julia Evans <julia@jvns.ca>
2024-01-09 22:38:20 -0500

    set http path

--- api/main.go
+++ api/main.go
@@ -272,6 +272,7 @@ func requireLogin(username string, page string, r *http.Request, w http.Response
 func (handle *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	ctx, span := tracer.Start(ctx, "http.request")
+	span.SetAttributes(attribute.String("http.path", r.URL.Path))
 	defer span.End()
 	logMsg(r, fmt.Sprintf("Request: %s", r.URL.Path))
 	w.Header().Set("Access-Control-Allow-Origin", "*")

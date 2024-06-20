06f9013b40fd1b5f7531f723436992a80538cc3b
Julia Evans <julia@jvns.ca>
2024-06-20 13:52:59 -0400

    update port

--- scripts/run_go.sh
+++ scripts/run_go.sh
@@ -5,4 +5,4 @@ go build
 cd .. || exit
 export DEV=true
 export POSTGRES_CONNECTION_STRING='postgres://postgres:mysecretpassword@localhost:5432/postgres?sslmode=disable'
-./api/mess-with-dns 5353
+./api/mess-with-dns 5354

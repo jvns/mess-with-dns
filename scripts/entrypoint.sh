e2a7192a00131a936d8bb715ace878dde8e5729d
Julia Evans <julia@jvns.ca>
2024-09-17 09:53:11 -0400

    try setting GOGC, see if it stops OOM kills

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,6 +5,7 @@ set -m
 
 export REQUEST_DB_FILENAME=/data/requests.sqlite
 export USER_DB_FILENAME=/data/users.sqlite
+export GOMEMLIMIT=250MiB
 
 pdns_server --config-dir=/etc/pdns &
 /usr/bin/mess-with-dns

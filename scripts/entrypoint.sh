d3104118d2b74056d55d127f0b24d46c4fa34439
Julia Evans <julia@jvns.ca>
2024-09-19 14:41:51 -0400

    try some more memory stuff

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -7,5 +7,9 @@ export REQUEST_DB_FILENAME=/data/requests.sqlite
 export USER_DB_FILENAME=/data/users.sqlite
 export GOMEMLIMIT=250MiB
 
+# idea from https://doc.powerdns.com/authoritative/performance.html#caches-memory-allocations-glibc
+# for reducing powerdns memory usage
+export MALLOC_ARENA_MAX=4
+
 pdns_server --config-dir=/etc/pdns &
 /usr/bin/mess-with-dns

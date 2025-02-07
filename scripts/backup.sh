e8bfa2fc739fa6975dcb1182f21dc7ac2f66c17b
Julia Evans <julia@jvns.ca>
2025-02-07 12:08:10 -0500

    stop backing up requests (in an attempt to reduce memory usage)

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -7,17 +7,15 @@ rm -f /tmp/*.sqlite.gz /tmp/*.sqlite
 #
 sqlite3 /data/powerdns.sqlite "VACUUM INTO '/tmp/powerdns.sqlite'"
 sqlite3 /data/users.sqlite "VACUUM INTO '/tmp/users.sqlite'"
-sqlite3 /data/requests.sqlite "VACUUM INTO '/tmp/requests.sqlite'"
 
 gzip /tmp/powerdns.sqlite
 gzip /tmp/users.sqlite
-gzip /tmp/requests.sqlite
 
 # Upload backup to S3
 export RESTIC_PASSWORD='notasecret'
 # try to reduce memory usage
 export GOGC=20
-restic -r s3://s3.amazonaws.com/wizardzines-db-backup/messwithdns/ backup /tmp/powerdns.sqlite.gz /tmp/requests.sqlite.gz /tmp/users.sqlite.gz
+restic -r s3://s3.amazonaws.com/wizardzines-db-backup/messwithdns/ backup /tmp/powerdns.sqlite.gz /tmp/users.sqlite.gz
 restic -r s3://s3.amazonaws.com/wizardzines-db-backup/messwithdns/ snapshots
 restic -r s3://s3.amazonaws.com/wizardzines-db-backup/messwithdns/ forget -l 1 -H 6 -d 2 -w 2 -m 2 -y 2
 restic -r s3://s3.amazonaws.com/wizardzines-db-backup/messwithdns/ prune

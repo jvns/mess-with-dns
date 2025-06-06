caad7bbe38f8e07163024a1aeb3988218d46c6c6
Julia Evans <julia@jvns.ca>
2025-06-06 16:30:54 -0400

    Track backups in Honeycomb

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -5,6 +5,8 @@ export REQUEST_DB_FILENAME=/data/requests.sqlite
 export USER_DB_FILENAME=/data/users.sqlite
 export GOMEMLIMIT=160MiB
 export MALLOC_ARENA_MAX=4
+export OTEL_EXPORTER_OTLP_ENDPOINT="https://api.honeycomb.io"
+export OTEL_EXPORTER_OTLP_HEADERS="x-honeycomb-team=${HONEYCOMB_API_KEY}"
 
 cleanup() {
    pkill -P $$
@@ -18,6 +20,7 @@ backup() {
         sleep 3h
         echo "Running hourly backup"
         bash /app/backup.sh
+        otel-cli exec --name "backup-process" -- bash /app/backup.sh
     done
 }
 
